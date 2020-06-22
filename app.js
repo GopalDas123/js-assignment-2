ce393adbc479c33ad0afc00bc01fd47445c6939a
GopalDas123 <Gopaldax@gmail.com>
2020-06-22 14:38:08 +0500

    First go

---
+++ app.js
@@ -0,0 +1,86 @@
+// Chapter 21-25 --- Task-1
+
+// let fN=prompt("First Name: ");
+// let lN=prompt("Last Name: ");
+// let fullName=fN+" "+lN;
+// alert("Hello " +fullName+ " Welcome Here");
+
+// Chapter 21-25 --- Task-2
+
+// let favPhone=prompt("Enter your favourite phone: ");
+// let len=favPhone.length
+// document.write("My Favourite phone is: "+favPhone);
+// document.write("<br>Length of String: "+len);
+
+// Chapter 21-25 --- Task-3
+
+// let a="Pakistani";
+// let n=a.indexOf('n');
+// document.write("String: "+a);
+// document.write("<br>Index of 'n': "+n);
+
+// Chapter 21-25 --- Task-4
+
+// let a="Hello World";
+// let b=a.lastIndexOf("l");
+// document.write("Last index of l: "+b);
+
+// Chapter 21-25 --- Task-5
+
+// let a="Pakistani";
+// let n=a.charAt(3);
+// document.write("String: "+a);
+// document.write("<br>Character At Index 3: "+n);
+
+// Chapter 21-25 --- Task-6
+
+// let fN=prompt("First Name: ");
+// let lN=prompt("Last Name: ");
+// let fullName=fN.concat(lN)
+// alert("Hello " +fullName+ " Welcome Here");
+
+// Chapter 21-25 --- Task-7
+
+// let a="Hyderabad";
+// let b=a.replace("Hyder","Islam");
+// document.write("Before: "+a)
+// document.write("<br>After: "+b)
+
+// Chapter 21-25 --- Task-8
+
+// let message ="Ali and Sami are best friends. They play cricket and football together.";
+// let repl = message.replace(/and/g, "&")
+// document.write(repl);
+
+// Chapter 21-25 --- Task-9
+
+// var a="472";
+// document.write("Value: "+a);
+// document.write("<br>Type: "+typeof(a));
+// var a=Number(a);
+// document.write("<br>Value: "+a);
+// document.write("<br>Type: "+typeof(a));
+
+// Chapter 21-25 --- Task-10
+
+// var a=prompt("Enter anything: ");
+// var b=a.toLocaleUpperCase();
+// document.write(b);
+
+// Chapter 21-25 --- Task-11
+
+// var a=prompt("Enter anything: ");
+// var b=a.toLocaleLowerCase();
+// var c=b.charAt(0).toLocaleUpperCase()+ a.substr(1).toLowerCase();
+// document.write("User input: " +a);
+// document.write("<br>Title Case: " +c);
+
+// Chapter 21-25 --- Task-12
+
+var a = 23.98;
+var b = a.toString();
+var c = b.replace(".", "");
+document.write("Number: " + a);
+document.write("<br>Result: " + c);
+
+// Chapter 21-25 --- Task-6
